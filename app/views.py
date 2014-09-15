a054f689a0854db530c752dd336d3e01bbdf9f78
Greg Novak <greg.novak@gmail.com>
2014-09-14 17:29:27 -0700

    Accept file upload via form post

--- app/views.py
+++ app/views.py
@@ -1,5 +1,17 @@
 from app import app
 
+import os.path
+
+import werkzeug
+from flask import render_template,request,redirect,url_for
+
+allowed_extensions = set(['jpg'])
+upload_folder = "/Users/novak/Desktop/Insight/app-pbot/app/uploads"
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in allowed_extensions
+
 @app.route('/')
 
 @app.route('/index')
@@ -7,3 +19,27 @@ def index():
     user = { } 
     return render_template("index.html", title="Blah")
     
+@app.route('/flask-upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = werkzeug.secure_filename(file.filename)
+            file.save(os.path.join(upload_folder, filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+    <p><input type=file name=file>
+    <input type=submit value=Upload>
+    </form>
+    '''
+
+@app.route('/uploaded_file')
+def uploaded_file():
+    blargh = request.args.get("filename")
+    return "Got file " + blargh
