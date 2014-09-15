2d535b2312ed672a7d2981add99b6af38e347d42
Greg Novak <greg.novak@gmail.com>
2014-09-14 18:07:07 -0700

    Add commented instructions for how to load data/models into flask app.

--- app/__init__.py
+++ app/__init__.py
@@ -2,3 +2,10 @@
 from flask import Flask
 app = Flask(__name__)
 from app import views
+
+# How to get at code/data
+# to load foo.py and app/bar.py, do this
+# 
+# import foo  # somewhat mysteriously works
+# import bar  # works
+# from app import bar  # also works
--- run.py
+++ run.py
@@ -1,4 +1,14 @@
 #!/opt/local/bin/python
 from app import app
 
+# How to get at code/data
+# to load foo.py and app/bar.py, do this
+#
+# import foo
+# from app import baz
+#
+# Beware, don't do "import app.baz" because this undoes Flask magic in
+# creating the app object, causing the following line to fail.
+
 app.run(debug = True)
+
